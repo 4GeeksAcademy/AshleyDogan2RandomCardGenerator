ef9731e6918c6e0629f2564b4804a023c469ba65
Ashley Dogan <a.a.rappette@gmail.com>
2025-01-09 01:07:33 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const symbols = ["♦", "♥", "♠", "♣"];
+  const numbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  let rdmNumber = Math.floor(Math.random() * numbers.length);
+
+document.querySelector(".center"); = 
+    number[rdmNumber];
+    
+  console.log(center);
+  console.log(numbers);
 };
